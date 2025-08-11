69824ad28682f6624b6142711b2d8bd906beafea
Nawaf <Nawaf.mahmood2005@gmail.com>
2025-08-11 19:33:58 -0400

    Updated warnings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import './globals.css';
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 
 const geistSans = Geist({
@@ -35,6 +37,7 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
       <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
